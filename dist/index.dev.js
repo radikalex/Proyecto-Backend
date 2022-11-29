3cc578f52e4ec8b14689308d3454ff01176f9375
Alex Jiménez <alexjimenezada@gmail.com>
2022-11-29 13:56:19 +0100

    Port changed

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -1,15 +1,17 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-const PORT = 3001
+"use strict";
 
-app.use(express.json(), cors())
+var express = require('express');
 
+var cors = require('cors');
+
+var app = express();
+var PORT = 3001;
+app.use(express.json(), cors());
 app.use('/users', require('./routes/users'));
 app.use('/products', require('./routes/products'));
 app.use('/categories', require('./routes/categories'));
 app.use('/orders', require('./routes/orders'));
 app.use('/reviews', require('./routes/reviews'));
-
-
-app.listen(PORT, () => console.log('Servidor levantado en el puerto ' + PORT))
+app.listen(PORT, function () {
+  return console.log('Servidor levantado en el puerto ' + PORT);
+});
